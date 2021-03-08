7e3c4c642327d4d7388fa5aadef0e7be919fa635
millers23 <millers23@up.edu>
2021-03-08 15:18:31 -0800

    Added Player and Card class skeletons

---
+++ app/src/main/java/com/example/gamestatehw/Card.java
@@ -0,0 +1,9 @@
+package com.example.gamestatehw;
+
+public class Card {
+    private int cost;
+
+    public Card(int c) {
+        cost = c;
+    }
+}
---
+++ app/src/main/java/com/example/gamestatehw/Player.java
@@ -0,0 +1,17 @@
+package com.example.gamestatehw;
+
+public class Player {
+    private int gold;
+    private int points;
+    private int numCards;
+    //private ArrayList<Card> hand;
+    //private Card character;
+
+    public Player() {
+        gold = 0;
+        points = 0;
+        numCards = 0;
+        //hand;
+        //character;
+    }
+}
