b659ba6c89eada2a97023ff55d0539933fa14869
mayhead24 <mayhead24@up.edu>
2021-03-10 15:30:36 -0800

    onClick

--- app/src/main/java/com/example/gamestatehw/MainActivity.java
+++ app/src/main/java/com/example/gamestatehw/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.gamestatehw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import java.util.ArrayList;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         GameState gameState = new GameState();
+        TextView editText = (TextView)findViewById(R.id.edittext);
+        Button buttonRunTest = (Button)findViewById(R.id.buttonRunTest);
+        buttonRunTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText("");
+                GameState firstInstance = new GameState();
+                GameState secondInstance = new GameState(firstInstance);
+
+                //editText.
+            }
+        });
     }
+
+
+
 }
